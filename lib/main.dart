59118066122badbd69402298833a35db0c0b6dc1
Panth Patel <panthhp@outlook.com>
2023-07-31 09:22:11 +0100

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
+import 'package:app_c20785/apis/firebase.dart';
 import 'package:app_c20785/firebase_options.dart';
 import 'package:app_c20785/pages/auth/auth.dart';
+import 'package:app_c20785/provider/provider.dart';
 import 'package:app_c20785/ui/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await auth.signOut();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  ThemeMode uiMode = ThemeMode.values.where((e) => e.name == (preferences.getString("uiMode") ?? "system")).single;
+  double tsf = preferences.getDouble("tsf") ?? 1.0;
+  runApp(
+    ChangeNotifierProvider<AppSettings>(
+      create: (_) => AppSettings(uiMode: uiMode, tsf: tsf),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,13 +28,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '20785C App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: theme.primary.colour),
-        useMaterial3: true,
+    return Consumer<AppSettings>(
+      builder: (context, appSettings, child) => MaterialApp(
+        title: '20785C App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: theme.primary.colour,
+            brightness: Brightness.light,
+          ),
+          fontFamily: "Roboto Mono",
+        ),
+        darkTheme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: theme.darkAppBar.colour,
+            brightness: Brightness.dark,
+            background: theme.darkSurface.colour,
+            surface: theme.darkSurface.colour,
+          ),
+          fontFamily: "Roboto Mono",
+          scaffoldBackgroundColor: theme.darkSurface.colour,
+        ),
+        themeMode: appSettings.uiMode,
+        home: const AuthPage(),
       ),
-      home: const AuthPage(),
     );
   }
 }
---
+++ lib/provider/provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+double tsfMin = 0.5;
+double tsfMax = 1.5;
+
+class AppSettings extends ChangeNotifier {
+  ThemeMode _uiMode = ThemeMode.system;
+  ThemeMode get uiMode => _uiMode;
+  double _tsf = 1.0;
+  double get tsf => _tsf;
+
+  Future<void> updateTSF(double newTSF) async {
+    assert(tsfMin <= tsf && tsf <= tsfMax);
+    _tsf = newTSF;
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setDouble("tsf", newTSF);
+    notifyListeners();
+  }
+
+  Future<void> updateUiMode(ThemeMode newUiMode) async {
+    _uiMode = newUiMode;
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString("uiMode", newUiMode.name);
+    notifyListeners();
+  }
+
+  AppSettings({required ThemeMode uiMode, required double tsf}) {
+    _uiMode = uiMode;
+    _tsf = tsf;
+  }
+}
